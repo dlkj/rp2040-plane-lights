1c03595f887008b12d6e83ba1d9e139fb8d04409
Daniel Kenyon-Jones <git@dlkj.co.uk>
2022-08-20 18:25:47 +0100

    Tidy includes, remove wrong comments.

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint;
-use hal::{prelude::_rphal_pio_PIOExt, Timer};
-use panic_probe as _;
-use rp_pico as bsp;
-
 use hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
+use hal::{prelude::_rphal_pio_PIOExt, Timer};
+use panic_probe as _;
+use rp_pico as bsp;
 use smart_leds::{SmartLedsWrite, RGB8};
 use ws2812_pio::Ws2812;
 
@@ -28,10 +27,8 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -57,8 +54,6 @@ fn main() -> ! {
 
     // Instanciate a Ws2812 LED strip:
     let mut ws = Ws2812::new(
-        // Use pin 6 on the Raspberry Pi Pico (which is GPIO4 of the rp2040 chip)
-        // for the LED data output:
         pins.gpio0.into_mode(),
         &mut pio,
         sm0,
