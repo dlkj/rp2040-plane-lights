46e241516a23e84e2d9c7591cd9ca3c5e16bb916
Daniel Kenyon-Jones <github@dlkj.co.uk>
2023-01-13 16:04:34 +0000

    dma write to ws2812

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,32 @@ use bsp::hal;
 use cortex_m::singleton;
 use defmt::*;
 use defmt_rtt as _;
+use fugit::HertzU32;
 use hal::{
     clocks::init_clocks_and_plls,
-    dma::{double_buffer, DMAExt},
+    dma::DMAExt,
+    gpio::{FunctionPio0, Pin, PinId},
     pac,
-    pio::PIOExt,
+    pio::{Buffers::OnlyTx, PIOExt},
     sio::Sio,
     watchdog::Watchdog,
 };
 use panic_probe as _;
 use rp_pico as bsp;
-use rp_pico::hal::gpio::{FunctionPio0, Pin};
-use rp_pico::hal::pio::Buffers::OnlyTx;
+use rp_pico::hal::dma::single_buffer;
+use rp_pico::hal::Clock;
+
+type LedPin = hal::gpio::bank0::Gpio0;
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    let _clocks = init_clocks_and_plls(
+    let clocks = init_clocks_and_plls(
         bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
@@ -45,54 +50,80 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let _led_pin: Pin<_, FunctionPio0> = pins.led.into_mode();
-    const LED_PIN_ID: u8 = 25;
+    let _led_pin: Pin<LedPin, FunctionPio0> = pins.gpio0.into_mode();
 
-    // HELLO WORLD in morse code:
-    // .... . .-.. .-.. --- / .-- --- .-. .-.. -..
-    #[allow(clippy::unusual_byte_groupings)]
-    let message = [
-        0b10101010_00100010_11101010_00101110,
-        0b10100011_10111011_10000000_10111011,
-        0b10001110_11101110_00101110_10001011,
-        0b10101000_11101010_00000000_00000000,
-        0b00000000_00000000_00000000_00000000,
-    ];
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let mut data = [0; 145];
+
+    for (i, x) in [
+        0x0f000000, 0x080f0000, 0x08000f00, 0x0f080000, 0x000f0000, 0x00080f00, 0x0f000800,
+        0x000f0800, 0x00000f00,
+    ]
+    .into_iter()
+    .cycle()
+    .take(data.len() - 1)
+    .enumerate()
+    {
+        data[i + 1] = x;
+    }
+
+    data[0] = 144 * 28u32;
 
     #[rustfmt::skip]
     let program = pio_proc::pio_asm!(
         ".wrap_target",
-        "    out pins, 1 [31]",
-        "    nop [31]",
+        "    out x, 32",
+        "next_bit:",        
+        "    mov pins, null [1]",
+        "    out pins, 1 [1]",
+        "    mov pins, !null",
+        "    jmp x-- next_bit",
+        "    mov pins, null"
+        "    set x, 31",
+        "end_pause:",
+        "    jmp x-- end_pause [8]"
         ".wrap");
+
+    //transfer time = 1.2 microseconds
+    const T1: u8 = 2; // start bit
+    const T2: u8 = 2; // data bit
+    const T3: u8 = 2; // stop bit
+    const CYCLES_PER_BIT: u32 = (T1 + T2 + T3) as u32;
+    const FREQ: HertzU32 = HertzU32::kHz(800);
+
+    let clock_freq = clocks.peripheral_clock.freq();
+
+    let bit_freq = FREQ * CYCLES_PER_BIT;
+    let int = clock_freq / bit_freq;
+    let rem = clock_freq - (int * bit_freq);
+    let frac = (rem * 256) / bit_freq;
 
     // Initialize and start PIO
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
     let installed = pio.install(&program.program).unwrap();
     let (mut sm, _rx, tx) = hal::pio::PIOBuilder::from_program(installed)
-        .out_pins(LED_PIN_ID, 1)
-        .clock_divisor_fixed_point(0, 0) // as slow as possible (0 is interpreted as 65536)
+        .out_pins(LedPin::DYN.num, 1)
+        .clock_divisor_fixed_point(int as u16, frac as u8)
         .autopull(true)
+        .pull_threshold(24)
         .buffers(OnlyTx)
         .build(sm0);
     // The GPIO pin needs to be configured as an output.
-    sm.set_pindirs([(LED_PIN_ID, hal::pio::PinDir::Output)]);
+    sm.set_pindirs([(LedPin::DYN.num, hal::pio::PinDir::Output)]);
     sm.start();
 
+    delay.delay_ms(10);
+
     let dma = pac.DMA.split(&mut pac.RESETS);
+    let ch0 = dma.ch0;
 
-    let tx_buf1 = singleton!(TX_BUFFER_1: [u32; 5] = message).unwrap();
-    let tx_buf2 = singleton!(TX_BUFFER_2: [u32; 5] = message).unwrap();
+    let tx_buf = singleton!(TX_BUFFER_1: [u32; 145] = data).unwrap();
 
-    let tx_transfer = double_buffer::Config::new((dma.ch0, dma.ch1), tx_buf1, tx).start();
-    let mut tx_transfer = tx_transfer.read_next(tx_buf2);
+    let tx_transfer = single_buffer::Config::new(ch0, tx_buf, tx).start();
+    let (ch0, tx_buf, tx) = tx_transfer.wait();
+    let tx_transfer = single_buffer::Config::new(ch0, tx_buf, tx).start();
+    let (_ch0, _tx_buf, _tx) = tx_transfer.wait();
 
-    loop {
-        // When a transfer is done we immediately enqueue the buffers again.
-        if tx_transfer.is_done() {
-            info!("Buffer done");
-            let (tx_buf, next_tx_transfer) = tx_transfer.wait();
-            tx_transfer = next_tx_transfer.read_next(tx_buf);
-        }
-    }
+    loop {}
 }
