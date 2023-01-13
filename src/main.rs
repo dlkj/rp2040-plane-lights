1b675a54382539e23dd121284910aa9a44bdbbf5
Daniel Kenyon-Jones <github@dlkj.co.uk>
2023-01-13 11:16:50 +0000

    remove all the leds bits for now

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,8 @@ use hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
-use hal::{prelude::_rphal_pio_PIOExt, Timer};
 use panic_probe as _;
 use rp_pico as bsp;
-use smart_leds::{SmartLedsWrite, RGB8};
-use ws2812_pio::Ws2812;
 
 #[entry]
 fn main() -> ! {
@@ -40,8 +37,6 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
-
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -49,95 +44,17 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
-
-    // Instanciate a Ws2812 LED strip:
-    let mut ws = Ws2812::new(
-        pins.gpio0.into_mode(),
-        &mut pio,
-        sm0,
-        clocks.peripheral_clock.freq(),
-        timer.count_down(),
-    );
-
-    const PORT_FORWARD: usize = 12;
-    const PORT_REAR: usize = 11;
-    const STARBOARD_FORWARD: usize = 4;
-    const STARBOARD_REAR: usize = 5;
-
-    const TAIL: usize = 8;
-    const BEACON: usize = 6;
-
-    let mut leds: [RGB8; 16] = [
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-        (0, 0, 0).into(),
-    ];
 
     let mut led_pin = pins.led.into_push_pull_output();
 
     loop {
         led_pin.set_high().unwrap();
 
-        leds[PORT_FORWARD] = (255, 0, 0).into();
-        leds[PORT_REAR] = (64, 64, 64).into();
-        leds[STARBOARD_FORWARD] = (0, 255, 0).into();
-        leds[STARBOARD_REAR] = (64, 64, 64).into();
-        leds[TAIL] = (64, 64, 64).into();
-        ws.write(leds.iter().copied()).unwrap();
-
-        delay.delay_ms(300);
-
-        leds[BEACON] = (255, 0, 0).into();
-        ws.write(leds.iter().copied()).unwrap();
-
-        delay.delay_ms(300);
-
-        leds[BEACON] = (0, 0, 0).into();
-        ws.write(leds.iter().copied()).unwrap();
-
         delay.delay_ms(300);
 
         led_pin.set_low().unwrap();
 
-        leds[PORT_FORWARD] = (255, 255, 255).into();
-        leds[PORT_REAR] = (255, 255, 255).into();
-        leds[STARBOARD_FORWARD] = (255, 255, 255).into();
-        leds[STARBOARD_REAR] = (255, 255, 255).into();
-        leds[TAIL] = (255, 255, 255).into();
-        ws.write(leds.iter().copied()).unwrap();
+        delay.delay_ms(300);
 
-        delay.delay_ms(33);
-
-        leds[PORT_FORWARD] = (255, 0, 0).into();
-        leds[PORT_REAR] = (64, 64, 64).into();
-        leds[STARBOARD_FORWARD] = (0, 255, 0).into();
-        leds[STARBOARD_REAR] = (64, 64, 64).into();
-        leds[TAIL] = (64, 64, 64).into();
-        ws.write(leds.iter().copied()).unwrap();
-
-        delay.delay_ms(33);
-
-        leds[PORT_FORWARD] = (255, 255, 255).into();
-        leds[PORT_REAR] = (255, 255, 255).into();
-        leds[STARBOARD_FORWARD] = (255, 255, 255).into();
-        leds[STARBOARD_REAR] = (255, 255, 255).into();
-        leds[TAIL] = (255, 255, 255).into();
-        ws.write(leds.iter().copied()).unwrap();
-
-        delay.delay_ms(33);
     }
 }
